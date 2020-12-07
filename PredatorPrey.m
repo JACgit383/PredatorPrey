9a451a974900a605c809e38aa2fbd4645416dfd5
Lucas Barnes <lucas.barnes2@gmail.com>
2020-12-07 11:26:51 -0700

    add equilibrium states

--- PredatorPrey.m
+++ PredatorPrey.m
@@ -7,12 +7,12 @@
 
 clear;close all;
 %solving numerically with ode45
-tstart=0;tfinal=50;
+tstart=0;tfinal=300;
 global a; a=1.1; %growth rate of prey population
 global alpha; alpha=.4; %predation rate on prey population
 global c; c=.4; %death rate of predator population
 global gamma; gamma=.1; %growth rate of predator population
-global r; r=0; %modifies the prey equation -> logistic growth expression
+global r; r=.01; %modifies the prey equation -> logistic growth expression
 u0=zeros(2,1); %create u0 columns
 u0(1)=10; u0(2)=10; %set up initial conditions for x(0) and y(0);
 
@@ -30,6 +30,7 @@ hold on
 plot(t,y1)
 legend('prey population','predator population')
 
+%% phase plots
 figure
 dxdt=(a.*x1-r.*(x1.^2))-alpha.*x1.*y1;
 plot(x1,dxdt)
@@ -55,3 +56,43 @@ plot(dxdt,dydt)
 title('Predator population growth vs. prey population growth')
 xlabel('Prey population growth')
 ylabel('Predator population growth')
+
+%% equilibrium conditions
+u0(1)=a/r; u0(2)=0; %set up initial conditions for x(0) and y(0);
+[t,u]=ode45(@rhsPPS,[tstart,tfinal],u0,options); %solve for values
+x1=u(:,1);y1=u(:,2); %get values for x and y
+
+%now plot x and y as functions of t
+plot(t,x1)
+xlabel('time')
+ylabel('population')
+title('Predator-Prey Populations Over Time')
+hold on
+plot(t,y1)
+legend('prey population','predator population')
+
+
+figure
+u0(1)=c/gamma; u0(2)=(a*gamma-c*r)/(alpha*gamma);
+[t,u]=ode45(@rhsPPS,[tstart,tfinal],u0,options); %solve for values
+x1=u(:,1);y1=u(:,2); %get values for x and y
+
+%now plot x and y as functions of t
+plot(t,x1)
+xlabel('time')
+ylabel('population')
+ylim([0 5])
+title('Predator-Prey Populations Over Time')
+hold on
+plot(t,y1)
+legend('prey population','predator population')
+
+
+
+
+
+
+
+
+
+
