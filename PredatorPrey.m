6b95ae97968725a5504ae281981c310441c3230d
Lucas Barnes <lucas.barnes2@gmail.com>
2020-12-08 17:15:37 -0700

    Add presentation plan

--- PredatorPrey.m
+++ PredatorPrey.m
@@ -5,16 +5,26 @@
 
 %a,c,alpha,gamma,r -> all positive constants
 
+% presentation ideas:
+% populations vs time, predator pop. vs. prey pop. quiver and trajectory
+% use same initial conditions for every plot
+% 1. explain the equations
+% 2. show plots of the solution without capacity factor
+% 3. show plots of the solution with capacity factor
+% 4. talk about the special case where y == x (c * (r + alpha) == a * gamma)
+
 clear;close all;
 %solving numerically with ode45
-tstart=0;tfinal=300;
-global a; a=1.1; %growth rate of prey population
-global alpha; alpha=.4; %predation rate on prey population
-global c; c=.4; %death rate of predator population
-global gamma; gamma=.1; %growth rate of predator population
-global r; r=.01; %modifies the prey equation -> logistic growth expression
+tstart=0;tfinal=30;
+global a; a=12; %growth rate of prey population
+global alpha; alpha=1; %predation rate on prey population
+global c; c=6; %death rate of predator population
+global gamma; gamma=2; %growth rate of predator population
+global r; r=3; %modifies the prey equation -> logistic growth expression
 u0=zeros(2,1); %create u0 columns
-u0(1)=10; u0(2)=10; %set up initial conditions for x(0) and y(0);
+u0(1)=1; u0(2)=1; %set up initial conditions for x(0) and y(0);
+
+
 
 options=odeset('RelTol',1e-8);
 
