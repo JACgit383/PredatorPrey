b90f79e08de3d52622c0120f7c4010f292acf968
JACgit383 <75452642+JACgit383@users.noreply.github.com>
2020-12-11 00:49:11 -0700

    Add Quiver Plots

--- PredatorPrey.m
+++ PredatorPrey.m
@@ -16,11 +16,11 @@
 clear;close all;
 %solving numerically with ode45
 tstart=0;tfinal=30;
-global a; a=12; %growth rate of prey population
-global alpha; alpha=1; %predation rate on prey population
-global c; c=6; %death rate of predator population
-global gamma; gamma=2; %growth rate of predator population
-global r; r=3; %modifies the prey equation -> logistic growth expression
+global a; a=1.1; %growth rate of prey population
+global alpha; alpha=0.4; %predation rate on prey population
+global c; c=0.4; %death rate of predator population
+global gamma; gamma=0.1; %growth rate of predator population
+global r; r=0; %modifies the prey equation -> logistic growth expression
 u0=zeros(2,1); %create u0 columns
 u0(1)=1; u0(2)=1; %set up initial conditions for x(0) and y(0);
 
@@ -40,6 +40,38 @@ hold on
 plot(t,y1)
 legend('prey population','predator population')
 
+%% QUIVER PLOTS
+%% special equilibrium a=12,alpha=1,c=6,gamma=2
+figure
+x=0:.6:10;y=x;
+[X,Y]=meshgrid(x,y);
+dxdt=(a*X-r*X.^2)-alpha*X.*Y; %prey equation
+dydt=(-c*Y)+gamma*X.*Y; %predator equation
+quiver(X,Y,dxdt,dydt)
+xx=0.1:.05:0.2; yy=9*ones(size(xx));
+streamline(X,Y,dxdt,dydt,xx,yy) %do several initial conditions
+hold on
+yy1=1:1.5:5; xx1=9.*ones(size(yy1));
+streamline(X,Y,dxdt,dydt,xx1,yy1)
+streamline(X,Y,dxdt,dydt,0.8,9)
+hold off
+axis equal
+xlabel('prey population')
+ylabel('predator population')
+title('Phase-Space for special equilibrium')
+%% simple baboon vs. cheetah: a=1.1,alpha=0.4,c=0.4,gamma=0.1,r=0
+figure
+x=0:.6:20;y=0:0.3:10;
+[X,Y]=meshgrid(x,y);
+dxdt=(a*X-r*X.^2)-alpha*X.*Y; %prey equation
+dydt=(-c*Y)+gamma*X.*Y; %predator equation
+quiver(X,Y,dxdt,dydt)
+yy=5:0.5:7; xx=9*ones(size(yy));
+streamline(X,Y,dxdt,dydt,xx,yy,[1e-3,1e5]) %do several initial conditions
+axis equal
+xlabel('prey population')
+ylabel('predator population')
+title('Baboon population vs. Cheetah population phase space')
 %% phase plots
 figure
 dxdt=(a.*x1-r.*(x1.^2))-alpha.*x1.*y1;
@@ -96,9 +128,6 @@ title('Predator-Prey Populations Over Time')
 hold on
 plot(t,y1)
 legend('prey population','predator population')
-
-
-
 
 
 
